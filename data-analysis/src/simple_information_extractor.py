36085285fa9e0e6a37ba3471b7f7b01de4f1f04e
Aleksander Drozd <aleksander.drozd@outlook.com>
2018-11-30 00:54:36 +0100

    Introduce structure for comparing metadata

---
+++ data-analysis/src/simple_information_extractor.py
@@ -0,0 +1,43 @@
+from src.information_extractor import InformationExtractor
+
+
+class SimpleInformationExtractor(InformationExtractor):
+
+    def extract_subject(self):
+        return None
+
+    def extract_rent(self):
+        return None
+
+    def extract_bills(self):
+        return None
+
+    def extract_deposit(self):
+        return None
+
+    def extract_internet_speed(self):
+        return None
+
+    def extract_district(self):
+        return None
+
+    def extract_street(self):
+        return None
+
+    def extract_rooms_count(self):
+        return None
+
+    def extract_flatmates_count(self):
+        return None
+
+    def extract_flatmates_gender(self):
+        return None
+
+    def extract_flatmates_occupation(self):
+        return None
+
+    def extract_preferred_occupation(self):
+        return None
+
+    def extract_preferred_gender(self):
+        return None
