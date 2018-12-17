039a61a741370f9ed00be99c476ba57dc7e2f494
Aleksander Drozd <aleksander.drozd@outlook.com>
2018-12-17 02:07:12 +0100

    Implement simple subject extration

--- data-analysis/src/simple_information_extractor.py
+++ data-analysis/src/simple_information_extractor.py
@@ -1,9 +1,27 @@
 from src.information_extractor import InformationExtractor
 
+import morfeusz2
+from src.morf_utils import MorfWrapper
+
 
 class SimpleInformationExtractor(InformationExtractor):
 
     def extract_subject(self, data):
+        morf = morfeusz2.Morfeusz()
+        analysis = morf.analyse(data)
+        morf_wrapper = MorfWrapper(analysis)
+
+        limit = 30
+
+        if morf_wrapper.contains_any(['jednoosobowy', '1os', '1-os', '1-osobowy'], limit)\
+                or (morf_wrapper.contains('pokój', limit) and not (morf_wrapper.contains('dwuosobowy', limit)
+                    or morf_wrapper.x_before_y('miejsce', 'w', limit)
+                    or morf_wrapper.x_follows_number('osobowy', 2, limit))):
+            return "room"
+        elif morf_wrapper.contains_any(['dwuosobowy', '2os', '2-os', '2-osobowy'], limit) \
+                or morf_wrapper.x_before_y('miejsce', 'w', limit) \
+                or morf_wrapper.x_follows_number('osobowy', 2, limit):
+            return "roomShare"
         return None
 
     def extract_rent(self, data):
