17637cd3d8cb47ccd545095cc05c3998d2efee41
Aleksander Drozd <aleksander.drozd@outlook.com>
2019-01-12 01:17:16 +0100

    Add new 50 files with data and fix existing

--- data-analysis/src/datasets.py
+++ data-analysis/src/datasets.py
@@ -3,9 +3,9 @@ import json
 import os
 
 
-attributes = ['subject', 'rent', 'bills', 'deposit', 'internetSpeed', 'district', 'street',
-              'roomsCount', 'flatmatesCount', 'flatmatesGenders',  'flatmatesOccupation',
-              'preferredOccupation',  'preferredGender']
+attributes = ['subject', 'flatMeterage', 'roomMeterage', 'rent', 'bills', 'deposit', 'internetSpeed',
+              'district', 'street', 'roomsCount', 'flatmatesCount', 'flatmatesGenders', 'flatmatesOccupation',
+              'preferredOccupation', 'preferredGender']
 
 
 def load(json_dir_name):
